a97d3edf6875b688caf39d2e33d1cfb3a58a1706
arturo32 <arturofonsecas32@gmail.com>
2022-10-26 10:02:51 -0300

    Improve formatting, add comments and a new condition
    A new condition for age was added. Now if the user is more than
    80 years old, a new message is displayed.

--- input.py
+++ input.py
@@ -1,12 +1,17 @@
-user_input = input ('Qual é o seu nome? ') 
+# This program asks the name and age of the user. Based on age, 
+# it displays different messages
 
-print ('Olá, ' + user_input + '. Prazer em te conhecer!')
+user_input = input( 'Qual é o seu nome? ' ) 
 
-user_age = int (input ('Me conta qual a sua idade? '))
+print( 'Olá, ' + user_input + '. Prazer em te conhecer!' )
+
+user_age = int( input( 'Me conta qual a sua idade? ' ) )
 
 if user_age < 10: 
-	print ('Que legal! Eu gosto muito do número ' + str (user_age) + '!')
+	print( 'Que legal! Eu gosto muito do número ' + str( user_age ) + '!' )
 
-else: 
-	print ('Hmmm, que interessante. Tenho certeza que irá compreender perfeitamente o que irei te explicar!')
+elif user_age < 70: 
+	print( 'Hmmm, que interessante. Tenho certeza que irá compreender perfeitamente o que irei te explicar!' )
 
+else:
+	print( 'Incrível! Também espero chegar aos ' + str( user_age ) + ' anos!' )
