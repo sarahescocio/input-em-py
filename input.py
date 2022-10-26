531029e8d091babd600a23531e3438a7a1f68f41
Sarah Escócio <114763074+sarahescocio@users.noreply.github.com>
2022-10-26 08:43:41 -0300

    First commit

---
+++ input.py
@@ -0,0 +1,12 @@
+user_input = input ('Qual é o seu nome? ') 
+
+print ('Olá, ' + user_input + '. Prazer em te conhecer!')
+
+user_age = int (input ('Me conta qual a sua idade? '))
+
+if user_age < 10: 
+	print ('Que legal! Eu gosto muito do número ' + str (user_age) + '!')
+
+else: 
+	print ('Hmmm, que interessante. Tenho certeza que irá compreender perfeitamente o que irei te explicar!')
+
